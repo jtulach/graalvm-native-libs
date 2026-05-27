b4cd6d7886e97164bb749129480501cbd9a20e4e
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2026-05-27 21:59:30 +0200

    Executes some LoadJvmTests in the native mode

---
+++ jvm/src/test/java/org/apidesign/jvm/channel/LoadJvmTest.java
@@ -0,0 +1,37 @@
+package org.apidesign.jvm.channel;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import org.graalvm.nativeimage.ImageInfo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/** Creates an instance of {@link JVM} and performs some tests on it.
+ * Please note: <strong>there can be only one HotSpot JVM</strong> per
+ * process. Avoid creating another ones.
+ */
+public final class LoadJvmTest {
+    @BeforeEach
+    public void initializeChannel() throws Exception {
+        Assumptions.assumeTrue(ImageInfo.inImageCode(), "Can only run in Native Image mode");
+    }
+
+    @Test
+    public void executeMainClass() throws Exception {
+        if (!ImageInfo.inImageRuntimeCode()) {
+            return;
+        }
+        var out = File.createTempFile("check-main", ".log");
+        for (var i = 0; i < 5; i++) {
+            var n = 10 + i;
+            TestUtils.jvm().executeMain("org/apidesign/jvm/channel/TestMain", out.getPath(), "" + n);
+            var content = Files.readString(out.toPath());
+            assertEquals(TestMain.factorial(n).toString(), content, "Factorial of " + n + " is the same");
+            out.delete();
+        }
+    }
+}
--- jvm/src/test/java/org/apidesign/jvm/channel/TestUtils.java
+++ jvm/src/test/java/org/apidesign/jvm/channel/TestUtils.java
@@ -2,13 +2,14 @@ package org.apidesign.jvm.channel;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import org.graalvm.nativeimage.ImageInfo;
 
 final class TestUtils {
     private TestUtils() {
     }
 
-    static final String PATH = System.getProperty("java.home");
-    static final String CLASS_PATH;
+    private static final String PATH = System.getProperty("java.home");
+    private static final String CLASS_PATH;
 
     static {
         try {
@@ -19,4 +20,15 @@ final class TestUtils {
         }
     }
 
+    private static JVM impl;
+    static JVM jvm() {
+        if (impl == null) {
+            assert ImageInfo.inImageRuntimeCode();
+            assert CLASS_PATH != null : "CLASS_PATH field must be set!";
+            var path = new File(PATH);
+            assert path.isDirectory() : "Java home exists: " + path;
+            impl = JVM.create(path, "-Djava.class.path=" + CLASS_PATH);
+        }
+        return impl;
+    }
 }
