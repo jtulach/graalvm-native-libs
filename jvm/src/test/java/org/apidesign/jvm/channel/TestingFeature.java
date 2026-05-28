4241fd9d76ddda495e165793fa0a614dcb45de0a
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2026-05-28 10:21:10 +0200

    Looks like native image is capable to deduce the need for reflective Conf constructor access by itself

--- jvm/src/test/java/org/apidesign/jvm/channel/TestingFeature.java
+++ jvm/src/test/java/org/apidesign/jvm/channel/TestingFeature.java
@@ -1,15 +1,11 @@
 package org.apidesign.jvm.channel;
 
 import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.hosted.RuntimeReflection;
 import org.graalvm.nativeimage.hosted.RuntimeSerialization;
 
 public final class TestingFeature implements Feature {
     @Override
     public void beforeAnalysis(BeforeAnalysisAccess access) {
-        RuntimeReflection.register(ChannelCountReturnTest.Conf.class);
-        RuntimeReflection.register(ChannelCountReturnTest.Conf.class.getConstructors());
-
         for (var clazz : ChannelMockInSingleJvmTest.class.getNestMembers()) {
             RuntimeSerialization.registerIncludingAssociatedClasses(clazz);
         }
