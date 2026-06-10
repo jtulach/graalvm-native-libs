29c8824b122b368baa749319661665d1cc9dd478
Jaroslav Tulach <jaroslav.tulach@apidesign.org>
2026-06-10 15:11:24 +0200

    Run project IDE action executes either in JVM or SVM if -Pnative

--- src/main/java/org/apidesign/demo/App.java
+++ src/main/java/org/apidesign/demo/App.java
@@ -5,6 +5,6 @@ package org.apidesign.demo;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello by " + System.getProperty("java.vm.name"));
     }
 }
